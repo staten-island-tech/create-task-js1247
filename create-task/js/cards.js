3e003dfee890cba3db346c9e35a841c6d406b200
js1247 <jasonshi1247@gmail.com>
2024-01-21 22:15:48 -0500

    1015

--- create-task/js/cards.js
+++ create-task/js/cards.js
@@ -1,10 +1,30 @@
 const cards = [
-    {
-        cardNumber: "1",
+  {
+    cardNumber: "2",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/English_pattern_2_of_spades.svg/800px-English_pattern_2_of_spades.svg.png",
+  },
+  {
+    cardNumber: "3",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/English_pattern_3_of_spades.svg/800px-English_pattern_3_of_spades.svg.png",
+  },
+  {
+    cardNumber: "4",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/English_pattern_4_of_spades.svg/800px-English_pattern_4_of_spades.svg.png",
+  },
+  { cardNumber: "5",},
+  { cardNumber: "6",},
+  { cardNumber: "7",},
+  { cardNumber: "8",},
+  { cardNumber: "9",},
+  { cardNumber: "10",},
+  { cardNumber: "J",},
+  { cardNumber: "Q",},
+  { cardNumber: "K",},
+  { cardNumber: "A",},
+];
 
-    },
-    {
-
-    }
-]
-export { cards }
+export { cards };
+//https://en.wikipedia.org/wiki/Standard_52-card_deck <--- Link to the card images
--- create-task/js/main.js
+++ create-task/js/main.js
@@ -1,3 +1,4 @@
 import { DOMSelectors } from "./dom";
 import "../css/style.css";
 import { cards } from "./cards"
+
