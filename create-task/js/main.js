5a09ceff7af24c73901978660ce31d3c0cb942f9
js1247 <jasonshi1247@gmail.com>
2024-02-01 09:22:07 -0500

    Update main.js

--- create-task/js/main.js
+++ create-task/js/main.js
@@ -1,3 +1,4 @@
 import { DOMSelectors } from "./dom";
 import "../css/style.css";
 import { cards } from "./cards"
+//remake blackjack into a simpler card game(simple war card game) where when they press the button draw it will give the player and the dealer a card and whoever gets the bigger card number wins, if they get the same cards it will make them draw again
